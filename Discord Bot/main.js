681356ac8bf968b739d297cefe420a998434cbe1
Eric <57415580+twojineric@users.noreply.github.com>
2020-07-25 19:47:05 -0700

    Added command handler

--- Discord Bot/main.js
+++ Discord Bot/main.js
@@ -1,17 +1,31 @@
-
+const fs = require ('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const bot = new Discord.Client();
+bot.commandsList = new Discord.Collection(); //Map (Collection) of commands
+var commandFiles = fs.readdirSync('./commands');
+var commandFiles = commandFiles.filter(file => file.endsWith('.js'));
+
+for(let file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+    bot.commandsList.set(command.name, command);
+}
+
+const keyword = '$echo' //$echo is the command word
+
+bot.once('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-client.on('message', async msg => {
-//$echo is the command word
-    if(msg.content.substring(0, 5) === '$echo') {
-        wordArray = msg.content.substring(5).split(' ');
-        switch(wordArray[1]) {
+bot.on('message', async msg => {
+
+    if(msg.content.substring(0, 5) === keyword && !msg.author.bot) {
+        argsArray = msg.content.substring(keyword.length).trim().split(' ');
+
+        switch(argsArray[0]) {
+
             case 'join':
             {
                 if(msg.member.voice.channel) {
@@ -25,7 +39,7 @@ client.on('message', async msg => {
             case 'leave':
             {
                 // check to see if it is in a channel already
-                if(msg.member.voice.channel && client.channels.cache.has(msg.member.voice.channel.id))
+                if(msg.member.voice.channel && bot.channels.cache.has(msg.member.voice.channel.id))
                     msg.member.voice.channel.leave();
                 break;
             }
@@ -48,15 +62,15 @@ client.on('message', async msg => {
             }
             case 'help':
             {
-                msg.channel.send('TBD');
+                bot.commandsList.get('help').execute(msg, argsArray);
                 break;
             }
             default:
-                'No matching command. Use $echo help'
+                msg.channel.send("Command not found, use $echo help")
         }
     }
 });
 
 // possibly obscure this later
 let token = "NzM0MTc0OTk4Njc5Mzg4MTkw.XxON7g.w8n4Haq_pcQomv70hWPMu19OUv";
-client.login(token + "I");
+bot.login(token + "I");
