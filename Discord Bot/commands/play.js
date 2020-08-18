e9c713859841c10c16a5d0517d61934937ec5acb
Eric <57415580+twojineric@users.noreply.github.com>
2020-08-17 21:16:58 -0700

    Minor bugfix

--- Discord Bot/commands/play.js
+++ Discord Bot/commands/play.js
@@ -1,10 +1,11 @@
 const ytdl = require('ytdl-core');
+const fs = require('fs');
 const command_messages = require('../command_messages.json');
 module.exports = {
     name: 'play',
     description: 'Streams a youtube video, audio only.',
     aliases: ['start'],
-    execute(msg, serverQueue){   
+    execute(msg, serverQueue){
         const disconnect = require('./disconnect.js');
         const queue = require('./queue.js');
 
@@ -16,7 +17,7 @@ module.exports = {
             queue.execute(msg, serverQueue);
             return;
         }
-        
+
         const channel = msg.member.voice.channel;
         const song = serverQueue.songs[0];
         const globalQueue = queue.globalQueue;
@@ -24,20 +25,23 @@ module.exports = {
         // check if song is defined. If it's not, the queue is empty.
         if(!song){
             disconnect.execute(msg); // disconnect will also delete the queue for us
-            msg.channel.send(command_messages.QUEUE_EMPTY);
             return;
         }else{
             // join and begin to play the song
             channel.join().then(connection => {
                 serverQueue.connection = connection;
                 msg.channel.send(`Playing: **${song.title}**`);
-                const stream = ytdl(song.url, {filter: 'audioonly'});
-                serverQueue.streamDispatcher = connection.play(stream);
+                serverQueue.streamDispatcher = connection.play(ytdl(song.url, {quality: 'highestaudio'}));
                 // when we're done, shift the array and play again until queue is empty
                 serverQueue.streamDispatcher.on('finish', () => {
                     msg.channel.send(command_messages.DONE_PLAYING_VIDEO);
                     serverQueue.songs.shift();
                     this.execute(msg, serverQueue);
+                });
+
+                serverQueue.streamDispatcher.on('error', error => {
+                    console.error(error);
+                    return;
                 });
             });
         }
--- Discord Bot/commands/search.js
+++ Discord Bot/commands/search.js
@@ -15,12 +15,10 @@ module.exports = {
 
         // pulls filters for the current search phrase
         ytsr.getFilters(searchTerm, function(err, filters){
-            // handle search error
             if(err){
                 msg.channel.send(command_messages.SEARCH_ERROR);
                 return;
             }
-
             var filter = filters.get('Type').find(obj => obj.name === 'Video');
 
             // construct object to hold our search options
@@ -32,8 +30,6 @@ module.exports = {
             // by passing in a null string, we indicate that we continue the previous search
             ytsr(null, options, function(err, searchResults){
                 const queue = require('./queue.js');
-
-                // handle search error
                 if(err){
                     msg.channel.send(command_messages.SEARCH_ERROR);
                     return;
@@ -41,7 +37,6 @@ module.exports = {
 
                 // retrieve the URL of the first video (of the 5 pulled)
                 var url = searchResults["items"][0]['link'].toString();
-                 
                 // as queue is meant to be used with a string array, we put our URL into a one-element array and pass it
                 queue.execute(msg, [url]);
                 return;
